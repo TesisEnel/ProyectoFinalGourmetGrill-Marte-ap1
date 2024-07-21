93f67e9ae66419e086f4f9ee2f5099efaf574f04
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-21 16:41:40 -0400

    Utilizando el OnModel Creating para guardar los estados de la orden, los métodos de pagos y las categorias

--- GourmetGrillApi.api/DAL/ApplicationDbContext.cs
+++ GourmetGrillApi.api/DAL/ApplicationDbContext.cs
@@ -19,4 +19,33 @@ public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     public DbSet<VentasDetalle> VentasDetalle { get; set; }
     public DbSet<Estados> Estados { get; set; }
     public DbSet<MetodoPagos> MetodoPagos { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        //ConfigureGeneralModel(modelBuilder);
+        //ConfigureProductosModel(modelBuilder);
+    }
+
+    public void ConfigureGeneralModel(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Estados>().HasData(
+             new Estados { EstadoId = 1, NombreEstado = "Pendiente" },
+             new Estados { EstadoId = 2, NombreEstado = "Preparando" },
+             new Estados { EstadoId = 3, NombreEstado = "YA ESTÁ PREPARADA" },
+             new Estados { EstadoId = 4, NombreEstado = "Cancelado" }
+        );
+        modelBuilder.Entity<MetodoPagos>().HasData(
+             new MetodoPagos { MetodoPagoId = 1, Nombre = "Efectivo" },
+             new MetodoPagos { MetodoPagoId = 2, Nombre = "Tarjeta" }
+        );
+        modelBuilder.Entity<CategoriaProductos>().HasData(
+             new CategoriaProductos { CategoriaId = 1, Nombre = "Hamburguesas" },
+             new CategoriaProductos { CategoriaId = 2, Nombre = "Papas" },
+             new CategoriaProductos { CategoriaId = 3, Nombre = "Acompañantes" },
+             new CategoriaProductos { CategoriaId = 4, Nombre = "Bebidas" }             
+        );
+    }
+
+   
 }
