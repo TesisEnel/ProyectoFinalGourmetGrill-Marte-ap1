13a6f46579c12ca7b317ae2ea66f683ed5478fea
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-21 13:10:01 -0400

    Creando DbContext heredando de IdentityDbContext<ApplicationUser>  que es una clase proporcionada por ASP.NET Core Identity para manejar la autenticación y autorización.

---
+++ GourmetGrillApi.api/DAL/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ProyectoFinalGourmetGrill.Data;
+using Shared.Models;
+
+namespace GourmetGrillApi.api.DAL;
+
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
+{
+    public DbSet<CategoriaProductos> CategoriaProductos { get; set; }
+    public DbSet<Productos> Productos { get; set; }
+    public DbSet<Ordenes> Ordenes { get; set; }
+    public DbSet<OrdenesDetalle> OrdenesDetalle { get; set; }
+    public DbSet<Ventas> Ventas { get; set; }
+    public DbSet<VentasDetalle> VentasDetalle { get; set; }
+    public DbSet<Estados> Estados { get; set; }
+    public DbSet<MetodoPagos> MetodoPagos { get; set; }
+}
