f83ca9a36df8bd6b8bfb6eea1c7a0afb274837fd
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-21 19:32:27 -0400

    Inyectando connectionstring y el Contexto en el program

--- GourmetGrillApi.api/Program.cs
+++ GourmetGrillApi.api/Program.cs
@@ -1,3 +1,6 @@
+using GourmetGrillApi.api.DAL;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,14 +10,18 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+var ConStr = builder.Configuration.GetConnectionString("ConStr") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(ConStr));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+//if (app.Environment.IsDevelopment())
+//{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+//}
 
 app.UseHttpsRedirection();
 
