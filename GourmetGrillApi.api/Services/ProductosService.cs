87a5a7b6f8578843ec73b97d75b6b6d5aba5220c
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-21 20:44:05 -0400

    Agregando Servicios del producto

---
+++ GourmetGrillApi.api/Services/ProductosService.cs
@@ -0,0 +1,57 @@
+﻿
+using GourmetGrillApi.api.DAL;
+using Microsoft.EntityFrameworkCore;
+using Shared.Interfaces;
+using Shared.Models;
+using System.Linq.Expressions;
+
+namespace ProyectoFinalGourmetGrill.Services;
+
+public class ProductosService(ApplicationDbContext _contexto) : IServer<Productos>
+{
+    public async Task<Productos> GetObject(int id)
+    {
+        return (await _contexto.Productos.FindAsync(id))!;
+    }
+
+    public async Task<List<Productos>> GetAllObject()
+    {
+        return await _contexto.Productos.ToListAsync();
+    }
+
+    public async Task<Productos> AddObject(Productos type)
+    {
+        _contexto.Productos.Add(type);
+        await _contexto.SaveChangesAsync();
+        return type;
+    }
+
+    public async Task<bool> UpdateObject(Productos type)
+    {
+        _contexto.Entry(type).State = EntityState.Modified;
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+
+    public async Task<Productos?> Search(int productoId)
+    {
+        return await _contexto.Productos.AsNoTracking().FirstOrDefaultAsync(a => a.ProductoId == productoId);
+    }
+
+    public async Task<bool> DeleteObject(int id)
+    {
+        var producto = await _contexto.Productos.FindAsync(id);
+        if (producto == null)
+        {
+            return false;
+        }
+        _contexto.Productos.Remove(producto);
+        return await _contexto.SaveChangesAsync() > 0;
+    }
+    public async Task<List<Productos>> GetObjectByCondition(Expression<Func<Productos, bool>> expression)
+    {
+        return await _contexto.Productos
+            .AsNoTracking()
+            .Where(expression)
+            .ToListAsync();
+    }
+}
