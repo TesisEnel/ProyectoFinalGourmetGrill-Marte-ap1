40dfd38e5527b0cd33eb2f9085f2ad1559c9edfa
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-24 01:26:45 -0400

    Inyectando Servicios en el program

--- GourmetGrillApi.api/Program.cs
+++ GourmetGrillApi.api/Program.cs
@@ -1,4 +1,6 @@
 using GourmetGrillApi.api.DAL;
+using GourmetGrillApi.api.Services;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using ProyectoFinalGourmetGrill.Services;
 using Shared.Interfaces;
@@ -18,7 +20,19 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
 
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 //Servicios
+builder.Services.AddScoped<IServerAsp<ApplicationUser>, UsersService>();
+builder.Services.AddScoped<IServerAsp<IdentityRole>, RolesService>();
+builder.Services.AddScoped<IServerAsp<IdentityUserRole<string>>, UserRolesService>();
+builder.Services.AddScoped<IServer<CategoriaProductos>, CategoriaProductosService>();
 builder.Services.AddScoped<IServer<Productos>, ProductosService>();
+builder.Services.AddScoped<IServer<Ordenes>, OrdenesService>();
+builder.Services.AddScoped<IServer<Ventas>, VentasService>();
+builder.Services.AddScoped<IServer<MetodoPagos>, MetodosPagosService>();
+builder.Services.AddScoped<IServer<Estados>, EstadosService>();
+builder.Services.AddScoped<IdentityUserService>();
+builder.Services.AddScoped<ProductosService>();
+builder.Services.AddScoped<MetodoPagos>();
+builder.Services.AddScoped<UsersService>();
 
 var app = builder.Build();
 
