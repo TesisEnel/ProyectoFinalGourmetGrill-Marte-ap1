d4a0c6bd7915d2fd7520da8b0e0ca3fc4c03b7de
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-07-27 20:27:51 -0400

    Agregando Precio a los modelos VentaDetalles y OrdenesDetalle

--- Shared/Models/OrdenesDetalle.cs
+++ Shared/Models/OrdenesDetalle.cs
@@ -22,6 +22,10 @@ public class OrdenesDetalle
     [Required(ErrorMessage = "Un Producto es requerido")]
     public Productos? Producto { get; set; }
 
+    [Required(ErrorMessage = "Indique el Precio")]
+    [Range(0.01, 1000000000, ErrorMessage = "El Precio debe estar 0.01 y 1000000000")]
+    public float Precio { get; set; }
+
     [Required(ErrorMessage = "Es requerido")]
     public int Cantidad { get; set; }
 }
