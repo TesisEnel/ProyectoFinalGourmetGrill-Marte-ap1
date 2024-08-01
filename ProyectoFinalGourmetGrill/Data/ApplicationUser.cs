7796c7e22caaffba86ac17f51385561c0a87e577
angelluisglez8@gmail.com <angelluisglez8@gmail.com>
2024-08-01 15:42:20 -0400

    Quitando Comentario

--- ProyectoFinalGourmetGrill/Data/ApplicationUser.cs
+++ ProyectoFinalGourmetGrill/Data/ApplicationUser.cs
@@ -2,9 +2,8 @@ using Microsoft.AspNetCore.Identity;
 
 namespace ProyectoFinalGourmetGrill.Data;
 
-	// Add profile data for application users by adding properties to the ApplicationUser class
-	public class ApplicationUser : IdentityUser
-	{
+public class ApplicationUser : IdentityUser
+{
     public string? Nombre { get; set; }
 
     public string? Apellido { get; set; }
