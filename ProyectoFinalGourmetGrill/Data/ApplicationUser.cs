301d1c6b3c975471dd57c948944b0ed8812b8cd6
DjMarte <alphat098@gmail.com>
2024-08-08 10:59:00 -0400

    Agrendo cosas faltantes

--- ProyectoFinalGourmetGrill/Data/ApplicationUser.cs
+++ ProyectoFinalGourmetGrill/Data/ApplicationUser.cs
@@ -6,8 +6,6 @@ public class ApplicationUser : IdentityUser
 {
     public string? Nombre { get; set; }
 
-    public string? Apellido { get; set; }
-
     public string? Cedula { get; set; }
 
     public string? NickName { get; set; }
